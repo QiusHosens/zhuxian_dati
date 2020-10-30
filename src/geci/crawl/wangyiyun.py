8723104f33750cf5be8047b1a404c6606fb174d5
zhangyingen <zhangyingen@chaoyingtec.com>
2020-10-30 13:18:35 +0800

    增加网易云歌单爬取和chrome driver

--- src/geci/crawl/wangyiyun.py
+++ src/geci/crawl/wangyiyun.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*
+# 改地址为用户歌单,暂不爬取
 # https://music.163.com/#/discover/playlist/?order=hot&cat=%E5%8D%8E%E8%AF%AD&limit=35&offset=105
 
 import bs4
@@ -12,7 +13,7 @@ from pyvirtualdisplay import Display
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome('/usr/local/Sunanang/chromedriver/chromedriver',
+driver = webdriver.Chrome('../../driver/win32/chromedriver.exe',
                           chrome_options=chrome_options)
 
 def getDriverHttp(url):
@@ -41,9 +42,24 @@ def get_zhuanji_url_list(soup, htmlClass):
     return hrefs
 
 if __name__=="__main__":
+    href_prefix = "https://music.163.com/#"
     offset = 105
     zhuanji_page_url = "https://music.163.com/#/discover/playlist/?order=hot&cat=%E5%8D%8E%E8%AF%AD&limit=35&offset=105"
-    zhuanji_page_resp_soup = getDriverHttp(zhuanji_page_url)
-    hrefs = get_zhuanji_url_list(zhuanji_page_resp_soup, 'm-pl-container')
-    print hrefs
+    driver.get(zhuanji_page_url)
+    zhuanji_iframes = driver.find_elements_by_tag_name('iframe')
+    # print iframes
+    zhuanji_iframe = zhuanji_iframes[0]
+    driver.switch_to.frame(zhuanji_iframe) # 最重要的一步
+    zhuanji_page_resp_soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
+    # zhuanji_page_resp_soup = getDriverHttp(zhuanji_page_url)
+    zhuanji_page_hrefs = get_zhuanji_url_list(zhuanji_page_resp_soup, 'm-pl-container')
+    print zhuanji_page_hrefs
+
+    for zhuanji_href in zhuanji_page_hrefs:
+        driver.get(zhuanji_href)
+        ge_page_frame = driver.find_elements_by_tag_name('iframe')[0]
+        driver.switch_to.frame(ge_page_frame)
+        ge_page_resp_soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        # TODO
